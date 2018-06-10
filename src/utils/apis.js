747d900aa018b40c24f97ede5923e1d94015ad65
Michael Fenwick <mickyfen17@aol.com>
2018-06-09 23:10:29 -0600

    loop & upload colored cluster images via Cloudinary & app to map

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,4 +1,4 @@
-// import mapInterface from './mapInterface';
+import mapInterface from './mapInterface';
 
 const apiCalls = {
   async airportsGeoJson() {
@@ -7,6 +7,29 @@ const apiCalls = {
     );
     const json = await response.json();
     return json;
+  },
+
+  loadImages() {
+    const images = [
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528564190/Images/airplane-shape-forestgrn.png',
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528569332/Images/airplane-shape-grassgrn.png',
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528569495/Images/airplane-shape-desert.png',
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528564190/Images/airplane-shape-grey.png',
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528564190/Images/airplane-shape-drkblue.png',
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1528564190/Images/airplane-shape-blue.png',
+    ];
+    return images.forEach((imgUrl, i) => {
+      mapInterface.renderedMap.loadImage(imgUrl, (error, image) => {
+        if (error) throw new Error(error);
+        const imageUrl = images[i];
+        const imageColor = imageUrl.substring(
+          imageUrl.lastIndexOf('-') + 1,
+          imageUrl.lastIndexOf('.')
+        );
+        mapInterface.renderedMap.addImage(`plane-${imageColor}`, image);
+        return image;
+      });
+    });
   },
 };
 
